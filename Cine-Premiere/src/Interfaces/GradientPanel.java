c834d1bdd66b405c06b87c019e73bd615086d736
LeonardoTamay <U23265290@utp.edu.pe>
2025-07-10 13:34:07 -0500

    Nuevos componentes

---
+++ Cine-Premiere/src/Interfaces/GradientPanel.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Interfaces;
+
+/**
+ *
+ * @author leona
+ */
+
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+public class GradientPanel extends JPanel {
+
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        
+        super.paintComponent(g);
+        
+        Graphics2D g2d = (Graphics2D) g;
+
+        GradientPaint gradient = new GradientPaint(
+            0, 0, new Color(10, 30, 100),      
+            getWidth(), getHeight(), new Color(90, 180, 255) 
+        );
+
+        g2d.setPaint(gradient);
+
+        g2d.fillRect(0, 0, getWidth(), getHeight());
+    }
+}
+
