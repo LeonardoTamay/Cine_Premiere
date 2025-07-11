b9f564da6df998f4e91ff7acf14a81b94eb8a857
Edu <educaceresruiz2005@gmail.com>
2025-07-10 23:46:59 -0500

    Agregue archivos y modifique clases

--- Cine-Premiere/src/Class/Administrador.java
+++ Cine-Premiere/src/Class/Administrador.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Clases_base;
+package Class;
 
 /**
  *
  * @author leona
  */
-public class Cliente {
+public class Administrador {
     
 }
--- Cine-Premiere/src/Enumeradores/TipoDocumento.java
+++ Cine-Premiere/src/Enumeradores/TipoDocumento.java
@@ -2,12 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Clases_base;
+package Enumeradores;
 
 /**
  *
- * @author leona
+ * @author USER
  */
-public class Administrador {
-    
+public enum TipoDocumento {
+    DNI, //documneto nacional de identidad
+    CE, //carnet de extranjeria
+    PASAPORTE
 }
